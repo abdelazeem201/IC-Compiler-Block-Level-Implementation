352add3a5d706986958b290e0ef71dcd93cabf3b
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2021-01-30 13:53:45 +0200

    Create block.tcl

---
+++ IC_Compiler_2016.03-SP1/ORCA_TOP/scripts_block/conf/block.tcl
@@ -0,0 +1,16 @@
+##########################################################################################
+# Lynx Compatible Reference Methodology (LCRM) Setup File
+# Script: block.tcl
+# Version: J-2014.09-SP2 (January 12, 2015)
+# Copyright (C) 2007-2015 Synopsys, Inc. All rights reserved.
+##########################################################################################
+
+## DESCRIPTION:
+## * This script is required to be present when running in Lynx. It is sourced 
+## * by lcrm_setup.tcl and can be optionally used to customize variables like 
+## * SVAR(tag#) as a mechanism to group metrics. Configuration is best done
+## * using the Lynx Runtime Manager block variable editor
+## -----------------------------------------------------------------------------
+
+
+
